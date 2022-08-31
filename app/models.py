7c4bbd22583facb899c3ffc9d287d9bd0f7371ab
Jagoda Jeczmien-Lazur <jagoda.jeczmienlazur@gmail.com>
2022-08-31 21:37:32 +0200

    fragnance_library_v2

---
+++ app/models.py
@@ -0,0 +1,25 @@
+from app import db
+
+
+class Brand (db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20), index=True, nullable=True)
+    surname = db.Column(db.String(20), index=True, nullable=True)
+    Fragnances = db.relationship("Fragnance", backref="brand", lazy="dynamic")
+
+    def __repr__(self):
+        return f"{self.name} {self.surname}"
+
+
+class Fragnance (db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20), index=True, nullable=True)
+    year = db.Column(db.Integer, index=True, nullable=True)
+    brand_id = db.Column(db.Integer, db.ForeignKey("brand.id"))
+
+    stock = db.Column(db.Boolean, default=False)
+
+    def __str__(self):
+        return f"{self.name}"
