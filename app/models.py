369f71cbb86d1bb8f2c25a49fb9dc6c959b7ce20
Jagoda Jeczmien-Lazur <jagoda.jeczmienlazur@gmail.com>
2022-09-01 20:09:47 +0200

    Part 2

--- app/models.py
+++ app/models.py
@@ -6,7 +6,7 @@ class Brand (db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(20), index=True, nullable=True)
     surname = db.Column(db.String(20), index=True, nullable=True)
-    Fragnances = db.relationship("Fragnance", backref="brand", lazy="dynamic")
+    fragnances = db.relationship("fragnance", backref="brand", lazy="dynamic")
 
     def __repr__(self):
         return f"{self.name} {self.surname}"
@@ -17,6 +17,7 @@ class Fragnance (db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(20), index=True, nullable=True)
     year = db.Column(db.Integer, index=True, nullable=True)
+    price = db.Column(db.Integer, index=True, nullable=True)
     brand_id = db.Column(db.Integer, db.ForeignKey("brand.id"))
 
     stock = db.Column(db.Boolean, default=False)
--- fragnanceshelf.py
+++ fragnanceshelf.py
@@ -1,1 +1,12 @@
-from app import app 
+from app import app, db
+from app.models import Brand, Fragnance
+
+
+@app.shell_context_processor
+def make_shell_context():
+    return {
+        "db": db,
+        "Brand": Brand,
+        "Fragnance": Fragnance
+    }
+
